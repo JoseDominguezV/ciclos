8e9b3bcb19485de21ce734957a862073c9cf7134
JoseDominguezV <dorance0320@gmail.com>
2022-06-25 08:36:35 -0500

    Ejemplo ciclo numeros

---
+++ src/numeros.java
@@ -0,0 +1,10 @@
+public class numeros {
+    public static void main(String[] args) {
+        //ejemplo escribir los numero del 1 al 100
+
+        for (int i=1; i<=100; i++){
+            System.out.println("Numero: "+i);
+        }
+
+    }
+}
