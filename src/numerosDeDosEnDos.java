98c32d140f8d41ee64f82416a9a26350b4d486ee
JoseDominguezV <dorance0320@gmail.com>
2022-06-25 08:47:48 -0500

    Ejemplo ciclo numeros de 2 en 2

---
+++ src/numerosDeDosEnDos.java
@@ -0,0 +1,10 @@
+public class numerosDeDosEnDos {
+    public static void main(String[] args) {
+        //Imprimir 10 numeros sumados de 2 en 2, empezando
+
+        for (int i=2; i<=20; i=i+2){
+            System.out.println("Numero: "+i);
+        }
+
+    }
+}
