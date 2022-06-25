1fc7002b98f0456167e28e68645e08dfb80ac849
JoseDominguezV <dorance0320@gmail.com>
2022-06-25 08:27:51 -0500

    Definicion ciclo for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        for (int i=0; i<3; i++){
+            JOptionPane.showMessageDialog(null, "Hola Mundo");
+            System.out.println("Hola Mundo");
+        }
 
     }
 }
