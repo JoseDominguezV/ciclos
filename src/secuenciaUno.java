7c2c251cf1307c8d2fb2b9d10b8483a5ea67c40e
JoseDominguezV <dorance0320@gmail.com>
2022-06-25 09:13:18 -0500

    Ejemplo secuenciaUno

---
+++ src/secuenciaUno.java
@@ -0,0 +1,11 @@
+public class secuenciaUno {
+    public static void main(String[] args) {
+        //Hacer que se vea en pantalla 1 3 1 3 1 3
+
+        for (int i=0; i<3; i++){
+            System.out.println("Numero: "+1);
+            System.out.println("Numero: "+3);
+        }
+
+    }
+}
